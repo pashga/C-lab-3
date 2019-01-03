4b451ffd3f49dcc233f5d43c5ad41a9e2121efb9
Victoria <alvi813@yandex.ru>
2019-01-03 04:44:56 +0300

    task3.cpp and main3.cpp were changed

--- src/task3.cpp
+++ src/task3.cpp
@@ -5,7 +5,50 @@
 #define IN 1
 #define OUT 0
 
+int getMaxWord(char buf[], char word[])
+{
+	int inWord = OUT; // are we in word (1) or not in word (0)
+	int letters = 0;
+	int max = 0;
+	char max_word[SIZE] = { '\0' };
+	int i = 0, j = 0, k = 0;
 
+
+
+	while (i < strlen(buf))
+	{
+		if (buf[i] != ' ' && inWord == OUT)
+		{
+
+			inWord = IN;
+			max_word[j] = buf[i];
+			j++;
+		}
+		else if (buf[i] != ' ' && inWord == IN)
+		{
+			max_word[j] = buf[i];
+			j++;
+		}
+		else if ((buf[i] == ' ' || buf[i] == '\0') && inWord == IN)
+		{
+			inWord = OUT;
+			j = 0;
+		}
+		if (strlen(max_word) > strlen(word))
+		{
+			int len = strlen(max_word);
+			for (k = 0; k <= len; k++)
+				word[k] = max_word[k];
+		}
+
+		i++;
+	}
+	return strlen(word);
+}
+
+
+
+/*
 #include "task3.h"
 #include <ctype.h> // for isspace()
 #include <stddef.h>
@@ -16,7 +59,7 @@ int getMaxWord(char buf[], char word[])
 	int inWord = 0; // are we in word (1) or not in word (0)
 	int letter_count = 0;
 	int max_letters = 0;
-	char * ptr = 0;
+	char * ptr = NULL;
 
 
 	for (int i = 0; buf[i]; i++)
@@ -42,8 +85,6 @@ int getMaxWord(char buf[], char word[])
 			}
 			letter_count = 0;
 		}
-		else
-			;
 	}
 
 	int i = 0;
@@ -56,3 +97,4 @@ int getMaxWord(char buf[], char word[])
 
 	return max_letters;
 }
+*/
