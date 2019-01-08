dd0f08fecc03b16e9af794590489c1c05d25c404
alekseypivovar <you@example.com>
2019-01-08 15:37:19 +0300

    Release 2

--- src/task6.cpp
+++ src/task6.cpp
@@ -6,19 +6,40 @@ int getSumMaxMin(int arr[], int N)
 {
 	int i = 1;
 	int min=arr[0];
+	int minNumber = 0;
 	int max=arr[0];
+	int maxNumber = 0;
+	int sum=0;
 
 	while (i < N)
 	{
 		if (arr[i] < min)
+		{
 			min = arr[i];
+			minNumber = i;
+		}
 
 		if (arr[i] > max)
+		{
 			max = arr[i];
-
-			i++;
+			maxNumber = i;
+		}
+		
+		i++;
 	}
+
+	if (minNumber < maxNumber)
+	{
+		for (int j = minNumber + 1; j < maxNumber; j++)
+			sum = sum + arr[j];
+	}
+	else
+	{
+		for (int j = maxNumber + 1; j < minNumber; j++)
+			sum = sum + arr[j];
+	}
+
 	printf("\nMinimum value = %d\nMaximum value = %d", min, max);
 	
-	return min+max;
+	return sum;
 }
