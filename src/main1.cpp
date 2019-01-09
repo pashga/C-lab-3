4e974286c2c5cf1f45fd1fb2461d575f783fae30
ipetrovanton <ipetrovanton@gmail.com>
2019-01-10 02:32:12 +0300

    ver. 1.2

--- src/main1.cpp
+++ src/main1.cpp
@@ -16,6 +16,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "task1.h"
 #define N 256
 
@@ -25,6 +26,9 @@ int main(){
     char buf[N];
     printf("Enter your text:\n");
     fgets(buf, N, stdin);
+    if (buf[strlen(buf) - 1] == '\n')
+        buf[strlen(buf) - 1] = '\0';
+    
     printf("Number of words is %d\n", wordCount(buf));
    
     
