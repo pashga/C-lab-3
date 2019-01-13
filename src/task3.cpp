c70125b8947fbf0dfeca4125d0db896820f666b6
lerkas <44975578+lerkas@users.noreply.github.com>
2019-01-13 17:06:54 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,7 +1,7 @@
 int getMaxWord(char buf[],char word[])
 
 {
-    int i, j, count, getMaxWord;
+    int i, j, count, maxWord;
     char theLongestWord[128];
 
     for(i = 0; buf[i] !='\n'; i++)                      //идём по строке пока не дойдём до конца
@@ -10,15 +10,15 @@ int getMaxWord(char buf[],char word[])
         {
             theLongestWord[count] = buf[i];           //заполняем массив первым самым длинным словом
             count++;
-            if (count > getMaxWord)
+            if (count > maxWord)
             {
-                getMaxWord = count;                     //определяем длину самого длинного слова
-                for(j = 0; j < getMaxWord; j++)
+                maxWord = count;                     //определяем длину самого длинного слова
+                for(j = 0; j < maxWord; j++)
                     word[j] = theLongestWord[j];        //помещаем в массив самое длинное слово
             }
         }
             else                                        //иначе при встрече с пробелом зануляем счётчик
                 count = 0;
     }
-    return word;
+    return maxWord;
 }
