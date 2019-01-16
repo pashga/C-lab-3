89d3a127ec774acb6fa4fb55c8a7417de2d92a68
Shalagin Nikolay <qquikly@mail.ru>
2019-01-16 11:33:44 +0300

    update task5

--- src/task5.cpp
+++ src/task5.cpp
@@ -1,14 +1,19 @@
-#include <stdio.h>
 int getSumInt(int arr[], int N)
 {
 	int end = N;
-	while (arr[end] < 0)
+	while (arr[end] < 0)//поиск последнего положительного
 		end--;
 	int start = 0;
-	while (arr[start] >= 0)
+	while (arr[start] >= 0)//поиск первого отрицательного
 		start++;
-	int summa = 0;
+	if (start > end)
+	{
+		int temp = end;
+		end = start;
+		start = temp;
+	}
 	start++;
+	int summa = 0;
 	while (start < end)
 	{
 		summa += arr[start];
